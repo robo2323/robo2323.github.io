d5d7546b7d9c006472bb0af38d0b49f6b3b8805a
Rob <rob.adam.h@gmail.com>
2017-02-18 14:26:53 +1100

    got rid of nav labels

--- scripts/main.js
+++ scripts/main.js
@@ -5,57 +5,18 @@ $( document ).ready(function() {
         $sectionContactOffset = $("#contact").offset().top,
         $navHeight = $('.navbar').height();
 
-/*    /!*function setNavCollapseOffset() {
-        var $winWidth = $(window).width();
-            if ( $winWidth > 950) {
-                $navCollapseOffset = "-2.5%";
-            } else if ($winWidth < 951 && $winWidth > 380) {
-                $navCollapseOffset = -1%;
-            }else if($winWidth < 381){
-                $navCollapseOffset = -5%;
 
-            }
-        }*!/
 
-    setNavCollapseOffset();*/
-
-        //show navbar labels on hover
-        $('.navbar-nav').hover(
-            function () {
-
-                //$('nav').css("margin-top", "0");
-                $('nav').css("height", $navHeight+"px");
-                $(".navbar-nav").css('margin', "0");
-
-                $('.navbar-name').css('top', '25%');
-                $(".navbar-nav p").css('display', 'block');
-
-            }, function () {
-                if ($(window).scrollTop() > 0) {
-
-                    $('nav').css("height", $navHeight/2+"px");
-                    $(".navbar-nav").css('margin', '-'+($navHeight/2.7)+"px");
-                    $(".navbar-nav p").css('display', 'none');
-                }
-
-            }
-        );
 
         //window resize handler
         $(window).resize(function () {
             //recalculate navbar height for scrolling function
             $navHeight = $('.navbar').height();
-            $(".navbar-nav").css('margin', "0");
-            $(".navbar-nav p").css('display', 'block');
-
-
         });
-
 
         //page scroll on nav clicks
         $('a.page-scroll').bind('click', function (event) {
             var $anchor = $(this);
-            //set
             if($anchor.attr('href')!="page-top") {
                 var $offset = $navHeight - $(window).height() * 0.04;
             }else{
@@ -76,19 +37,14 @@ $( document ).ready(function() {
 
             if ($scrollPos < 10) {
                 $("nav").css("background-color", "rgba(34, 34, 34, 0.1)");
-                $('nav').css("height", $navHeight+"px");
-                $(".navbar-nav").css('margin', "0");
 
-                $('.navbar-name').css('top', '25%');
-                $(".navbar-nav p").css('display', 'block');
+
 
                 $('.fa-arrow-up').css('visibility', 'hidden');
             }
             if ($scrollPos > 9) {
                 $('.fa-arrow-up').css('visibility', 'visible');
-                $('nav').css("height", $navHeight/2+"px");
-                $(".navbar-nav").css('margin', '-'+($navHeight/2.7)+"px");
-                $(".navbar-nav p").css('display', 'none');
+
             }
             if ($scrollPos > 10 && $scrollPos < $sectionContactOffset - $navHeight) {
                 $("nav").css("background-color", "rgba(34, 34, 34, 1)");
